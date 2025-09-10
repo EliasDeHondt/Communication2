3177e720b0fe1da085bf3a9cee8af9a87b2e6085
Elias De Hondt <Elias.dehondt@outlook.com>
2025-09-10 11:23:28 +0200

    Update vlog-3.php

--- assets/pages/vlog-3.php
+++ assets/pages/vlog-3.php
@@ -7,11 +7,11 @@
         <title>BierBrigade - Vlog 3</title>
         <meta content="width=device-width, initial-scale=1.0" name="viewport">
         <meta property="og:title" content="BierBrigade - Vlog 3" />
-        <meta property="og:image" content="https://eliasdh.com/assets/media/images/logo-hd.png"/>
+        <meta property="og:image" content="https://eliasdh.comhttps://eliasdehondt.github.io/Communication2/assets/media/images/logo-hd.png"/>
         <meta content="BierBrigade - Vlog 3" name="description">
         <!--Meta + Title-->
         <!--Favicon-->
-        <link href="/assets/media/images/Bier_Brigade_Logo.png" rel="icon">
+        <link href="https://eliasdehondt.github.io/Communication2/assets/media/images/Bier_Brigade_Logo.png" rel="icon">
         <!--Favicon-->
         <!--Google Web Fonts-->
         <link rel="preconnect" href="https://fonts.gstatic.com">
@@ -21,11 +21,11 @@
         <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
         <!--Font Awesome-->
         <!--Libraries Stylesheet-->
-        <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+        <link href="lib/owlcarouselhttps://eliasdehondt.github.io/Communication2/assets/owl.carousel.min.css" rel="stylesheet">
         <link href="lib/lightbox/css/lightbox.min.css" rel="stylesheet">
         <!--Libraries Stylesheet-->
         <!--Custom CSS-->
-        <link rel="stylesheet" href="/assets/css/style.css">
+        <link rel="stylesheet" href="https://eliasdehondt.github.io/Communication2/assets/css/style.css">
         <!--Custom CSS-->
     </head>
     <body class="selectioncoler" data-spy="scroll" data-target=".navbar" data-offset="51">
@@ -65,12 +65,12 @@
         <!--JavaScript-->
         <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.bundle.min.js"></script>
-        <script src="/assets/js/main.js"></script>
-        <script src="/assets/js/script-1.js"></script>
-        <script src="/assets/js/get-comments.js"></script>
+        <script src="https://eliasdehondt.github.io/Communication2/assets/js/main.js"></script>
+        <script src="https://eliasdehondt.github.io/Communication2/assets/js/script-1.js"></script>
+        <script src="https://eliasdehondt.github.io/Communication2/assets/js/get-comments.js"></script>
         <script>
-            loadHTML("top-navbar", "/assets/includes/top-navbar.html");
-            loadHTML("bottom", "/assets/includes/bottom.html");
+            loadHTML("top-navbar", "https://eliasdehondt.github.io/Communication2/assets/includes/top-navbar.html");
+            loadHTML("bottom", "https://eliasdehondt.github.io/Communication2/assets/includes/bottom.html");
         </script>
         <!--JavaScript-->
     </body>
